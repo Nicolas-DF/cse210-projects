25d2bba08c351a932f0e739db0c5e8eaca718544
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-06 10:17:50 -0300

    Exercise completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 100);
+
+        Console.WriteLine("Magic Number Game!");
+        bool done = false;
+
+        do
+        {
+            Console.Write("Whats is your guess? ");
+            string guess = Console.ReadLine();
+            int number = int.Parse(guess);
+
+            if (number > MagicNumber)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else if (number < MagicNumber)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else
+            {
+                done = true;
+            }
+
+        } while (done == false);
+
+        Console.WriteLine("You guessed it!!!");
+
+
     }
 }
