88eef8655e65df7e1a3abe6ded1969db706539e3
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-06 10:21:09 -0300

    Fixed random generator parameters

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int MagicNumber = randomGenerator.Next(1, 100);
+        int MagicNumber = randomGenerator.Next(1, 101);
 
         Console.WriteLine("Magic Number Game!");
         bool done = false;
