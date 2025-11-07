215dfdd512c096a3a2e05bc24eb1f64bc204be40
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-07 11:34:27 -0300

    Completed the class and its methods

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -19,5 +19,12 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2025;
         job2.Display();
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
