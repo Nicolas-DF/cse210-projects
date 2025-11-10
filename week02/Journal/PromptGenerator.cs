fd44ad39479606626c7bec3be54a1b8f669287a6
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-10 07:52:28 -0300

    Completed the file

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>
+    {
+        "What was the best part of your day?",
+        "What did you learn today?",
+        "How did you see the hand of the Lord in your life today?",
+        "What are you grateful for today?",
+        "What made you smile today?"
+
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
