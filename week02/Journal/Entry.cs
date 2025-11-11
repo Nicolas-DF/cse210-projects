400c7166723068a273c950d6ff08c9455575ebca
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-11 10:31:22 -0300

    Completed the pre modeled Entry.cs methods and functions

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -7,8 +7,15 @@ public class Entry
     public string _promptText;
     public string _entryText;
 
+    public Entry(string date, string promptText, string entryText)
+    {
+        _date = date;
+        _promptText = promptText;
+        _entryText = entryText;
+    }
     public void Display()
     {
-        
+        Console.WriteLine($"Date: {_date} – Prompt: {_promptText}");
+        Console.WriteLine($"{_entryText}\n");
     }
 }
