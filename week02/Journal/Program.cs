f8258e19609da23271706fcb3255d306b077535c
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-11 13:22:43 -0300

    Completed the program and fixed indentation

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,51 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         Journal journal = new Journal();
         PromptGenerator promptGenerator = new PromptGenerator();
         int choice = 0;
         while (choice != 5)
         {
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.Write("What would you like to do? ");
-        choice = int.Parse(Console.ReadLine());
-        
-        if (choice == 1)
+            DateTime theCurrentTime = DateTime.Now;
+            string date = theCurrentTime.ToShortDateString();
+
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = int.Parse(Console.ReadLine());
+            
+            if (choice == 1)
             {
                 string prompt = promptGenerator.GetRandomPrompt();
                 Console.WriteLine(prompt);
                 string response = Console.ReadLine();
+                Entry newEntry = new Entry(date, prompt, response);
+                journal.AddEntry(newEntry);
             }
-        else if (choice == 2)
+            else if (choice == 2)
             {
-                
+                journal.DisplayAll();
             }
-        else if (choice == 3)
-            {
-                Console.WriteLine("What is the file name?");
-                string fileName = Console.ReadLine();
-
-            }
-        else if (choice == 4)
+            else if (choice == 3)
             {
                 Console.WriteLine("What is the file name?");
                 string fileName = Console.ReadLine();
-                
+                journal.LoadFromFile(fileName);
             }
-        else if (choice == 5)
+            else if (choice == 4)
+            {
+                Console.WriteLine("What is the file name?");
+                string fileName = Console.ReadLine();
+                journal.SaveToFile(fileName);
+            }
+            else if (choice == 5)
             {
                 Console.WriteLine("Goodbye!");
             }
-        else
+            else
             {
                 Console.WriteLine("Sorry, this is not a valid option.");   
             }
-        
         }
     }
 }
