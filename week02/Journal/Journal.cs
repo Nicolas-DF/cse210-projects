5e2a2006cef16ebfaf8e2f631d4d27799f862094
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-11 10:59:22 -0300

    Journal.cs completed. It displays and saves according to Entry formatting

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -7,18 +7,33 @@ public class Journal
 
     public void AddEntry(Entry newEntry)
     {
-        
+        _entries.Add(newEntry);
     }
     public void DisplayAll()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
     public void SaveToFile(string file)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine(entry.ToFileFormat());
+            }
+        }
     }
     public void LoadFromFile(string file)
     {
-        
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(file);
+        foreach (string line in lines)
+        {
+            Entry entry = Entry.FromFileFormat(line);
+            _entries.Add(entry);
+        }
     }
 }
