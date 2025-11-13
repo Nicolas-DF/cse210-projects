f46d76f8e1781048349efc048052abf785cf7cc6
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-13 11:10:57 -0300

    Created the clas, private attibutes and the constructors

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,5 +1,38 @@
 using System;
 
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _bottom = 1;
+        _top = 1;
+    }
+    public Fraction(int wholeNumber)
+    {  
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 class Program
 {
     static void Main(string[] args)
