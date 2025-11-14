df082c54a55ce4400501cdac75e2128b29ddbacd
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-14 10:38:14 -0300

    Set the skeleton of Scripture.cs

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,26 @@
+
+using System.Runtime.CompilerServices;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference Reference, string text)
+    {
+        
+    }
+
+    public void HideRandomNumbers(int numberToHide)
+    {
+        
+    }
+    public void GetDisplayText() // change to string
+    {
+        
+    }
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
