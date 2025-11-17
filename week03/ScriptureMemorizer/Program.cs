e5d6939d930e38332b2dfc1f336a517c4f1cd8f6
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-17 14:18:13 -0300

    Wrote the loop to make the program works

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,6 +6,20 @@ class Program
     {
         Reference reference = new Reference("Proverbs", 3, 5, 6);
         Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge Him, and He shall direct thy paths.");
-        
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("\n Press enter to continue or type 'quit' to finish:");
+            string response = Console.ReadLine();
+            if (response == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomNumbers(3);
+        }
     }
 }
