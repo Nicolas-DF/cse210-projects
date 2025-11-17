d0176ec55d4c1b048e8c7259b83334e4e46c91de
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-17 14:03:26 -0300

    Finished Scripture.cs methods

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,4 +1,5 @@
-
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Runtime.CompilerServices;
 
 public class Scripture
@@ -8,19 +9,51 @@ public class Scripture
 
     public Scripture(Reference Reference, string text)
     {
-        
+        _reference = Reference;
+        _words = new List<Word>();
+
+        if (string.IsNullOrEmpty(text))
+        {
+            return;
+        }
+
+        // Spliting into simple spaces. Each piece turns into a token.
+        // Preserves words with comma (ex: "knowledge,")
+        var tokens = text.Split(' ');
+
+        foreach (var token in tokens)
+        {
+            _words.Add(new Word(token));
+        }
+
     }
 
     public void HideRandomNumbers(int numberToHide)
     {
-        
+        int count = 3;
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        Random random = new Random();
+        List<Word> mixed = visibleWords.OrderBy(w => random.Next()).ToList();
+
+        for (int i = 0; i < count && i < mixed.Count; i++)
+        {
+            mixed[i].Hide();
+        }
     }
-    public void GetDisplayText() // change to string
+    public string GetDisplayText()
     {
-        
+        return string.Join(" ", _words.Select(w => w.GetDisplayText()));
     }
     public bool IsCompletelyHidden()
     {
-        return false;
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
