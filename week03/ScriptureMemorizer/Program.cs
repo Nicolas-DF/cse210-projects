6425ecdb0fec700ddb887728bdc5b7243aa24a40
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-18 09:37:16 -0300

    Added am exceeding requirement - memorizing mode (and explanation)

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Reflection;
 class Program
 {
     static void Main(string[] args)
     {
         Reference reference = new Reference("Proverbs", 3, 5, 6);
         Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge Him, and He shall direct thy paths.");
+
+        //Asking to user the "mode", that makes the memorizing process easier or harder.
+        Console.Write("Do you want the easy (E) mode or spicy mode (S)? ");
+        string mode = Console.ReadLine();
+        
 
         while (true)
         {
@@ -25,7 +31,15 @@ class Program
                 break;
             }
 
-            scripture.HideRandomNumbers(3);
+            // Propositally making the answer to be easy or not. The spicy mode goes on if the user doesn't ask for the easy mode
+            if (mode.ToLower() == "e" || mode.ToLower() == "easy")
+            {
+                scripture.HideRandomNumbers(3);
+            }
+            else
+            {
+                scripture.HideRandomNumbers(6);
+            }
         }
     }
 }
