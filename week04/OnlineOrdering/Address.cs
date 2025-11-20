e8b58f471595e43f2f914c4b69b581e7a871b319
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-20 12:05:57 -0300

    Address.cs for Address class is half=way there

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateORprovince;
+    private string _country;
+
+    // constructor(s)
+
+    public bool IsInUSA()
+    {
+        if (_country == "USA" || _country == "US" || _country == "United States")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void GetAddress()
+    {
+        Console.WriteLine($""); 
+    }
+
+}
