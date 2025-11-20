df7362f0ec8b680d14d585f278add3c8531343f9
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-20 13:38:09 -0300

    Completed Customer.cs methods, attributes and constructor

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -3,13 +3,16 @@
 public class Customer
 {
     private string _name;
-    private Address address = new Address();
+    private Address _address;
 
-
-
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
     public void LivesInUSA()
     {
-        if (address.IsInUSA())
+        if (_address.IsInUSA())
         {
             Console.WriteLine("The customer lives in the USA");
         }
@@ -18,5 +21,12 @@ public class Customer
             Console.WriteLine("The costumer lives not in the USA");
         }
     }
-
+    public string GetName()
+    {
+        return _name;
+    }
+    public void GetCustomerAddress()
+    {
+        _address.GetAddress();
+    }
 }
