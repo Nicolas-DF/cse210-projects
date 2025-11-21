4f61eb9641cbbb76473cfeef821835007b1b96d3
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-21 11:55:01 -0300

    Completed Order.cs attributes, consturctor and methods

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -2,12 +2,42 @@
 
 public class Order
 {
-    public void GetPackingLabel()
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
     {
-        
+        _customer = customer;
+        _products = new List<Product>();
     }
-    public void GetStringLabel()
+
+    public void AddProduct(Product product)
     {
-        
+        _products.Add(product);
+    }
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label\n";
+        foreach (Product p in _products)
+        {
+            label += $"{p.GetName()} – (ID: {p.GetID()})\n";
+        }
+        return label;
+    }
+    public string GetShippingLabel()
+    {
+        return $"Shipping label:\n{_customer.GetName()}\n{_customer.GetCustomerAddress()}";
+    }
+    public double GetTotalPrice()
+    {
+        double total = 0;
+        foreach (Product p in _products)
+        {
+            total += p.GetTotalCost();
+        }
+        if (_customer.LivesInUSA())
+            return total += 5;
+        else
+            return total += 35;
     }
 }
