1b4e6df1b900d57353fe6e9c79d5651d06cf4ed9
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-26 16:57:40 -0300

    Completed the MathAssignment example

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -6,5 +6,9 @@ class Program
     {
         Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
         Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
