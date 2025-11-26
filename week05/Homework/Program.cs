6cd020a15821e96e322fcb7e3c0a1746333ea425
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-26 16:38:29 -0300

    Completed first test for the Assignment class

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
     }
 }
