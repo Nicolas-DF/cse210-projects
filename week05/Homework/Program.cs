cd9f1095a3af542e085e5c1d8b612d1cb0c28706
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-26 17:05:15 -0300

    Completed the WrittingAssignment sample test

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -10,5 +10,9 @@ class Program
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        WrittingAssignment writtingAssignment = new WrittingAssignment("The Causes of World War II", "Mary Waters", "European History");
+        Console.WriteLine(writtingAssignment.GetSummary());
+        Console.WriteLine(writtingAssignment.GetWrittingInfo());
     }
 }
