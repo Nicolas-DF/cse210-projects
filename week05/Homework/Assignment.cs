ef1d4dd2a34f5353a41683fd5a4e4305d4fc0587
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-26 16:38:04 -0300

    Created the Assignment class with its method, constructor and attributes

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,24 @@
+
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+
+    public Assignment()
+    {
+        _studentName = "Anonymous";
+        _topic = "Unknown";
+    }
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} – {_topic}";
+    }
+}
