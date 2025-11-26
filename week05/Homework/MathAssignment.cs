307550ea610e5f256f4d8e5b834b4447da6cee92
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-26 16:55:47 -0300

    Setted the class' constructor and method

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -5,5 +5,16 @@ public class MathAssignment : Assignment
     private string _textBookSection;
     private string _problems;
 
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems)
+    {
+        _problems = problems;
+        _textBookSection = textBookSection;
+        _studentName = studentName;
+        _topic = topic;
+    }
 
+    public string GetHomeworkList()
+    {
+        return $"Section {_textBookSection} Problems {_problems}";
+    }
 }
