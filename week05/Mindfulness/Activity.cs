c9d76f29960651f8501f9997a65a1001d5a2f32a
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-27 17:45:46 -0300

    Methods completed

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -4,28 +4,47 @@ public class Activity
 {
     private string _name;
     private string _description;
-    private int _duration;
+    protected int _duration;
 
-    public Activity(string name, string description, int duration)
+    public Activity(string name, string description)
     {
         _name = name;
         _description = description;
-        _duration = duration;
     }
 
     public void DisplayStartingMessage()
-    {    
+    {
+        Console.WriteLine($"Welcome to the {_name}");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+        
+        
     }
     public void DisplayEndingMessage()
     {
-        
+        Console.WriteLine("\nWell done!!");
     }
     public void ShowSpinner(int seconds)
     {
-        
+        char[] spinnerChars = { '|', '/', '—', '\\' };
+        for (int i = 0; i < seconds * 4; i++)
+        {
+            Console.Write(spinnerChars[i % spinnerChars.Length]);
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+        }
     }
     public void DisplayCountDown(int seconds)
     {
-        
+        while (seconds > 0)
+        {
+            Console.Write(seconds);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+            seconds--;
+        }
     }
 }
