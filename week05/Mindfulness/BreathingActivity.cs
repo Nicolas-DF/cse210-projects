c598ee429220dc0461b7755c92538c529b9cf5e0
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-11-27 17:47:18 -0300

    Created the BreathingActivity class

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,28 @@
+
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
+    {
+    }
+
+    public void Start()
+    {
+        Console.Clear();
+        DisplayStartingMessage();
+        Console.WriteLine("Get ready to begin...");
+        ShowSpinner(3);
+
+        int halfDuration = _duration / 2;
+        for (int i = 0; i < halfDuration; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            DisplayCountDown(4);
+            Console.WriteLine("Breathe out...");
+            DisplayCountDown(6);
+        }
+
+        DisplayEndingMessage();
+        ShowSpinner(3);
+    }
+}
