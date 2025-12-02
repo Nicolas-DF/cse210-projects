d127abd5a07919f7f61e604ddc2eef6af4a022c4
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-02 11:51:40 -0300

    Completed the tests about the classes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Circle circle = new Circle(5);
+        circle.SetColor("Brown");
+        Console.WriteLine($"Circle color: {circle.GetCOlor()}");
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine();
+        Rectangle rectangle = new Rectangle(4, 6);
+        rectangle.SetColor("Blue");
+        Console.WriteLine($"Rectangle color: {rectangle.GetCOlor()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        Console.WriteLine();
+        Square square = new Square(3);
+        square.SetColor("Red");
+        Console.WriteLine($"Square color: {square.GetCOlor()}");
+        Console.WriteLine($"Square area: {square.GetArea()}");
     }
 }
