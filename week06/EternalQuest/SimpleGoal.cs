0729582d171a348f6312ee8b1674201b566de4bb
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-04 11:49:52 -0300

    Setted the class, it's attributes, constructor and some methods

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,26 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+        _isComplete = true;
+    }
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+    public override string GetStringRepresentation()
+    {
+        return $"Simple Goal|{_shortName}|{_description}|{_points}|{_isComplete}";
+    }
+
+}
