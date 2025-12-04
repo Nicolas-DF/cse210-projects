e4943cbb24d48272a3457ee71a7f9025eeddb796
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-04 17:04:50 -0300

    Created the class, the attributes and the names for the methods

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,42 @@
+public abstract class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+    public GoalManager()
+    {
+        _goals = new List<Goal>();
+    }
+
+    public void Start()
+    {
+        
+    }
+    public void DisplayPlayerInfo()
+    {
+        
+    }
+    public void ListGoalNames()
+    {
+        
+    }
+    public void ListGoalDetails()
+    {
+        
+    }
+    public void CreateGoal()
+    {
+        
+    }
+    public void RecordEvent()
+    {
+        
+    }
+    public void SaveGoals()
+    {
+        
+    }
+    public void LoadGoals()
+    {
+        
+    }
+}
