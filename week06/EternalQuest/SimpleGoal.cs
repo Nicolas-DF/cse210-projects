b3aefe5d5ea12612a46148e20f7801f1715ccbb9
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-05 21:02:24 -0300

    Constructor was redundant. Made it simplier.

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -4,9 +4,6 @@ public class SimpleGoal : Goal
 
     public SimpleGoal(string name, string description, int points) : base(name, description, points)
     {
-        _shortName = name;
-        _description = description;
-        _points = points;
         _isComplete = false;
     }
 
