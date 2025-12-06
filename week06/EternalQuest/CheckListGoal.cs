4c570c7407a9da9e1b1a4203a5c1a7a578b31f19
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-05 21:06:51 -0300

    Simplified the constructor and added the _amountCompleted attirbute

--- week06/EternalQuest/CheckListGoal.cs
+++ week06/EternalQuest/CheckListGoal.cs
@@ -6,11 +6,9 @@ public class CheckListGoal : Goal
 
     public CheckListGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
     {
-        _shortName = name;
-        _description = description;
-        _points = points;
         _target = target;
         _bonus = bonus;
+        _amountCompleted = 0;
     }
 
     public override void RecordEvent()
