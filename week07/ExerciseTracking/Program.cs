b1420f545f5611ba75a7a23a6979a82099e3026f
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-09 10:32:40 -0300

    Created one activity of each type, added to a list, and displayed the summary of each one

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Set culture to invariant to ensure consistent number formatting
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+
+        List<Activity> activities = new List<Activity>();
+
+        Activity running = new Running("09 Dec 2025", 30, 4800);
+        activities.Add(running);
+        Activity cycling = new Cycling("08 Dec 2025", 45, 15);
+        activities.Add(cycling);
+        Activity swimming = new Swimming("07 Dec 2025", 60, 30);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
