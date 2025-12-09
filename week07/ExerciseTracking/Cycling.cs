8f50e068e8300ed12e92ddadba525e041ef2ee20
Nicolas-DF <nicolas.dfalco@gmail.com>
2025-12-09 09:47:46 -0300

    Declared class's specific attributes and constructor

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,30 @@
+
+
+public class Cycling : Activity
+{
+    private double _speed; // in kph
+
+    public Cycling(string date, int length, double speed) : base(date, length)
+    {
+        _speed = speed;
+    }
+
+    public double GetSpeedValue()
+    {
+        return _speed;
+    }
+    public override double GetDistance()
+    {
+        return (_speed * GetLength()) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+}
